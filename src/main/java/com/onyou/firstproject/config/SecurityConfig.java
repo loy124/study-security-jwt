fe77d2a8e8f27fd1de8c10c363d0bfec19be9b47
leeonyou <onyou.lee@mincoding.co.kr>
2023-05-04 15:33:02 +0900

    init

---
+++ src/main/java/com/onyou/firstproject/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.onyou.firstproject.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .build();
+
+    }
+}
